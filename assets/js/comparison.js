4ef34c15e7ab998fc15b457a2ce0446141496d73
Eric Pang <eric.pang@live.com>
2017-01-09 23:21:45 -0500

    Replaced / to per

--- assets/js/comparison.js
+++ assets/js/comparison.js
@@ -71,22 +71,22 @@ app.controller("chequingController", ["$scope", function($scope) {
   $scope.banks = [
       //TD
       { imgsrc: "assets/images/TD.png", name1: "Youth Savings", name2: "Account", requirement: "12+",
-          bankTrans: "25 free", nonBankFees: "$2.00/",
+          bankTrans: "25 free", nonBankFees: "$2.00 per",
           nonBankTrans: "transaction", numATMS: 73,
           link: "http://www.tdcanadatrust.com/products-services/banking/accounts/chequing-accounts/everyday.jsp"},
       //RBC
       { imgsrc: "assets/images/RBC.png", name1: "Leo's Young", name2: "Savers Account", requirement: "13+",
-          bankTrans: "25 free", nonBankFees: "$1.50/",
+          bankTrans: "25 free", nonBankFees: "$1.50 per",
           nonBankTrans: "transaction", numATMS: 49,
           link: "http://www.rbcroyalbank.com/products/deposits/student-banking.html"},
       //CIBC
       { imgsrc: "assets/images/CIBC.png", name1: "Advantage", name2: "for Youth", requirement: "18+",
-          bankTrans: "None", nonBankFees: "$1.50/",
+          bankTrans: "None", nonBankFees: "$1.50 per",
           nonBankTrans: "transaction", numATMS: "61 + PC Financial ATMs",
           link: "https://www.cibc.com/en/personal-banking/ways-to-bank/ways-to-pay/advantage-debit-card.html"},
       //Scotiabank
       { imgsrc: "assets/images/Scotiabank.png", name1: "Getting There", name2: "", requirement: "18+",
-          bankTrans: "None", nonBankFees: "$1.50/",
+          bankTrans: "None", nonBankFees: "$1.50 per",
           nonBankTrans: "transaction", numATMS: "37",
           link: "http://www.scotiabank.com/ca/en/0,,60,00.html"},
       //National Bank
@@ -96,17 +96,17 @@ app.controller("chequingController", ["$scope", function($scope) {
           link: "https://www.nbc.ca/en/personal/banking/accounts-and-packages/youth-chequing-account/coolcash.html"},
       //PC Financial
       { imgsrc: "assets/images/PCFinancial.png", name1: "Interest Plus", name2: "Savings Account", requirement: "18+",
-          bankTrans: "Unlimited", nonBankFees: "$1.50/",
+          bankTrans: "Unlimited", nonBankFees: "$1.50 per",
           nonBankTrans: "transaction", numATMS: "14 + CIBC ATMs",
           link: "http://www.banking.pcfinancial.ca/mkt/bankaccounts/nofeebankaccount-en.html"},
       //Tangerine
       { imgsrc: "assets/images/Tangerine.png", name1: "Children's", name2: "Savings Account", requirement: "16+",
-          bankTrans: "Unlimited", nonBankFees: "$1.00/",
+          bankTrans: "Unlimited", nonBankFees: "$1.00 per",
           nonBankTrans: "transaction", numATMS: "Scotiabank ATMS",
           link: "https://www.tangerine.ca/en/spending/chequing-account/index.html"},
       //ATB
       { imgsrc: "assets/images/ATB.png", name1: "Generation Account", requirement: "University Students",
-          bankTrans: "Unlimited", nonBankFees: "$1.50/",
+          bankTrans: "Unlimited", nonBankFees: "$1.50 per",
           nonBankTrans: "transaction", numATMS: 0,
           link: "https://get.atb.com/Personal/Bank/Student-Accounts/Students-First-Account/p/2307"}
   ];
