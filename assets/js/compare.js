ec9f23bae262476f17f81796380b38b8c84be160
ericpang777 <eric.pang@live.com>
2017-02-15 23:59:23 -0500

    Add additional sorting

--- assets/js/compare.js
+++ assets/js/compare.js
@@ -7,54 +7,54 @@ app.controller("savingController", ["$scope", function($scope) {
     //list of banks
     $scope.banks = [
         //ATB
-        { imgsrc: "assets/images/compare/ATB.png", name1: "Generation", name2: "Account", rate: 0.10, requirement: "Under 19",
-            eTransfer: "$1.50 per", eTrans: "transaction", nonBankFees: "$1.50 per",
-            nonBankTrans: "transaction",
+        { imgsrc: "assets/images/compare/ATB.png", name1: "Generation", name2: "Account", rate: 0.10,
+            eTransfer: "$1.50 per", eTrans: "transaction", nonBankFees: "$1.50 per", atmRank: 3,
+            nonBankTrans: "transaction", prep: "Over", branches: 200,
             link: "https://get.atb.com/Personal/Bank/Youth-Accounts/Generation-Account/p/2315"},
         //BMO
-        { imgsrc: "assets/images/compare/BMO.png", name1: "Smart Saver", name2: "", rate: 0.40, requirement: "Under 19",
-            eTransfer: "Unlimited", eTrans: "", nonBankFees: "$1.50 per",
-            nonBankTrans: "transaction",
+        { imgsrc: "assets/images/compare/BMO.png", name1: "Smart Saver", name2: "", rate: 0.40,
+            eTransfer: "Unlimited", eTrans: "", nonBankFees: "$1.50 per", atmRank: 4,
+            nonBankTrans: "transaction", prep: "Over", branches: 960,
             link: "https://www.bmo.com/main/personal/bank-accounts/smart-saver/"},
         //CIBC
-        { imgsrc: "assets/images/compare/CIBC.png", name1: "Advantage", name2: "for Youth", rate: 0.15, requirement: "Under 19",
-            eTransfer: "$1.50 per", eTrans: "transaction", nonBankFees: "$1.50 per",
-            nonBankTrans: "transaction",
+        { imgsrc: "assets/images/compare/CIBC.png", name1: "Advantage", name2: "for Youth", rate: 0.15,
+            eTransfer: "$1.50 per", eTrans: "transaction", nonBankFees: "$1.50 per", atmRank: 5,
+            nonBankTrans: "transaction", prep: "", branches: 1108,
             link: "https://www.cibc.com/en/personal-banking/bank-accounts/youth-banking-offers.html"},
         //CWB
-        { imgsrc: "assets/images/compare/CWB.png", name1: "Youth Account", name2: "", rate: 0.05, requirement: "Under 19",
-            eTransfer: "$1.50 per", eTrans: "transaction", nonBankFees: "$1.50 per",
-            nonBankTrans: "transaction",
+        { imgsrc: "assets/images/compare/CWB.png", name1: "Youth Account", name2: "", rate: 0.05,
+            eTransfer: "$1.50 per", eTrans: "transaction", nonBankFees: "$1.50 per", atmRank: 6,
+            nonBankTrans: "transaction", prep: "Over", branches: 40,
             link: "http://www.cwbank.com/personal-banking/bank-accounts/youth-account"},
         //National Bank
-        { imgsrc: "assets/images/compare/NationalBank.png", name1: "Strategist", name2: "Account", rate: 0.02, requirement: "18 years old",
-            eTransfer: "$1.25 per", eTrans: "transaction", nonBankFees: "$0.75 per",
-            nonBankTrans: "transaction",
-            link: "https://www.nbc.ca/en/personal/banking/saving-accounts/strategist-account.html"},
+        { imgsrc: "assets/images/compare/NationalBank.png", name1: "Strategist", name2: "Account", rate: 0.02,
+            eTransfer: "$1.25 per", eTrans: "transaction", nonBankFees: "$0.75 per", atmRank: 2,
+            nonBankTrans: "transaction", prep: "Around", branches: 450,
+            link: "https://www.rbc.ca/en/personal/banking/saving-accounts/strategist-account.html"},
         //PC Financial
-        { imgsrc: "assets/images/compare/PCFinancial.png", name1: "Interest Plus", name2: "Savings Account", rate: 0.85, requirement: "18 years old",
-            eTransfer: "$1.50 per", eTrans: "transaction", nonBankFees: "$1.50 per",
-            nonBankTrans: "transaction",
+        { imgsrc: "assets/images/compare/PCFinancial.png", name1: "Interest Plus", name2: "Savings Account", rate: 0.85,
+            eTransfer: "$1.50 per", eTrans: "transaction", nonBankFees: "$1.50 per", atmRank: 7,
+            nonBankTrans: "transaction", prep: "", branches: 0,
             link: "http://www.banking.pcfinancial.ca/mkt/bankaccounts/interestplussavingsaccount-en.html"},
         //RBC
-        { imgsrc: "assets/images/compare/RBC.png", name1: "Leo's Young", name2: "Savers Account", rate: 0.01, requirement: "Under 19",
-          eTransfer: "Unlimited", eTrans: "", nonBankFees: "$1.50 per",
-          nonBankTrans: "transaction",
+        { imgsrc: "assets/images/compare/RBC.png", name1: "Leo's Young", name2: "Savers Account", rate: 0.01,
+          eTransfer: "Unlimited", eTrans: "", nonBankFees: "$1.50 per", atmRank: 8,
+          nonBankTrans: "transaction", prep: "Around", branches: 1200,
           link: "http://www.rbcroyalbank.com/products/deposits/leo-young-savers-account.html"},
         //Scotiabank
-        { imgsrc: "assets/images/compare/Scotiabank.png", name1: "Getting There", name2: "", rate: 0.10, requirement: "Under 19",
-            eTransfer: "$1.00 per", eTrans: "transaction", nonBankFees: "$1.50 per",
-            nonBankTrans: "transaction",
+        { imgsrc: "assets/images/compare/Scotiabank.png", name1: "Getting There", name2: "", rate: 0.10,
+            eTransfer: "$1.00 per", eTrans: "transaction", nonBankFees: "$1.50 per", atmRank: 9,
+            nonBankTrans: "transaction", prep: "", branches: 980,
             link: "http://www.scotiabank.com/ca/en/0,,61,00.html"},
         //Tangerine
-        { imgsrc: "assets/images/compare/Tangerine.png", name1: "Children's", name2: "Savings Account", rate: 0.90, requirement: "Under 19",
-            eTransfer: "N/A", eTrans: "", nonBankFees: "N/A",
-            nonBankTrans: "",
+        { imgsrc: "assets/images/compare/Tangerine.png", name1: "Children's", name2: "Savings Account", rate: 0.90,
+            eTransfer: "N/A", eTrans: "", nonBankFees: "N/A", atmRank: 1,
+            nonBankTrans: "", prep: "", branches: 0,
             link: "https://www.tangerine.ca/en/saving/savings-accounts/childrens-savings-account/index.html"},
         //TD
-        { imgsrc: "assets/images/compare/TD.png", name1: "Youth Savings", name2: "Account", rate: 0.05, requirement: "Under 19",
-            eTransfer: "$1.50 per", eTrans: "transaction", nonBankFees: "$2.00 per",
-            nonBankTrans: "transaction",
+        { imgsrc: "assets/images/compare/TD.png", name1: "Youth Savings", name2: "Account", rate: 0.05,
+            eTransfer: "$1.50 per", eTrans: "transaction", nonBankFees: "$2.00 per", atmRank: 10,
+            nonBankTrans: "transaction", prep: "Over", branches: 1150,
             link: "https://www.tdcanadatrust.com/products-services/banking/accounts/savings-accounts/youth.jsp"}
     ];
 }]);
@@ -111,42 +111,42 @@ app.controller("cellController", ["$scope", function($scope) {
   $scope.companies = [
       //Bell
       { imgsrc: "assets/images/compare/bell.png", name1: "Bell", name2: "", plan: "Initial Device - Unlimited anytime nationwide minutes + 2 GB (Share Plan)",
-          minutes: "Unlimited in Canada", texts: "Unlimited in Canada",
+          minutes: "Unlimited in Canada", texts: "Unlimited in Canada", callRank: 4, textRank: 3,
           data: 2, price: 80, bonuses: "call display, message center, call waiting, conference calling",
           link: "https://www.bell.ca/Business/Mobility/Rate-Plans/Share_plans"},
       //Chatr
       { imgsrc: "assets/images/compare/chatr.png", name1: "Chatr", name2: "(Formerly Mobilicity)", plan: "$45 per month Talk, Text, Data Plan",
-          minutes: "Unlimited to Canada (in zone)", texts: "Unlimited (in zone)",
+          minutes: "Unlimited to Canada (in zone)", texts: "Unlimited (in zone)", callRank: 2, textRank: 1,
           data: 2, price: 45, bonuses: "voicemail, international talk saver, call display, call forwarding, group calling, unlimited incoming text",
           link: "http://www.chatrwireless.com/web/chatr.portal?_nfpb=true&_pageLabel=PlanBrowse&s_cid=base:plans:en"},
       //Fido
       { imgsrc: "assets/images/compare/fido.png", name1: "Fido", name2: "", plan: "3 GB Data, Talk and Text Plan",
           minutes: "1000 International Long Distance Minutes (to 8 selected countries) / Unlimited in Canada and USA", texts: "Unlimited in Canada and USA",
-          data: 3, price: 60, bonuses: "circle calling, call display, voicemail",
+          callRank: 8, textRank: 5, data: 3, price: 60, bonuses: "circle calling, call display, voicemail",
           link: "https://www.fido.ca/nac/#/choose-plan"},
       //Freedom
       { imgsrc: "assets/images/compare/freedom.png", name1: "Freedom", name2: "(Formerly Wind)", plan: "Smartphone Plan 1",
-          minutes: "Unlimited to Canada", texts: "Unlimited to Canada and USA",
+          minutes: "Unlimited to Canada", texts: "Unlimited to Canada and USA", callRank: 3, textRank: 4,
           data: 2, price: 35, bonuses: "call control, voicemail, still use data at reduced speed after using all full-speed data, 15 cents per minute to USA (calls), 5 cents per text to other countries",
           link: "http://www.freedommobile.ca/plans-and-devices/plans/mobile-plan-details/smartphone-plan-35"},
       //Koodo
       { imgsrc: "assets/images/compare/koodo.png", name1: "Koodo", name2: "", plan: "1 GB Data + 1 GB Shock-Free Data",
-          minutes: "Unlimited in Canada", texts: "Unlimited",
+          minutes: "Unlimited in Canada", texts: "Unlimited", callRank: 5, textRank: 6,
           data: 2, price: 55, bonuses: "call display, voicemail, call waiting, conference calling",
           link: "https://www.koodomobile.com/rate-plans"},
       //Rogers
       { imgsrc: "assets/images/compare/rogers.png", name1: "Rogers", name2: "", plan: "1 GB + 1 GB Share Everything Plan",
-          minutes: "Unlimited in Canada", texts: "Unlimited",
+          minutes: "Unlimited in Canada", texts: "Unlimited", callRank: 6, textRank: 7,
           data: 2, price: 90, bonuses: "call display and name display, group calling, call waiting, 2500 call forwarding minutes, worry-free data management, enhanced voicemail, roam like home feature, Rogers NHL GameCenter LIVE",
           link: "http://www.rogers.com/consumer/wireless/smartphone-plans#seeFullDetailsText"},
       //Telus
       { imgsrc: "assets/images/compare/telus.png", name1: "Telus", name2: "", plan: "Elite 67",
-          minutes: "500 anytime local minutes", texts: "Unlimited incoming/outgoing international SMS and MMS",
+          minutes: "500 anytime local minutes", texts: "Unlimited incoming/outgoing international SMS and MMS", callRank: 1, textRank: 8,
           data: 1, price: 67, bonuses: "voicemail 3, conference calling, call waiting, call display, can auto-renew each 30 days",
           link: "http://www.telus.com/en/on/mobility/prepaid/plans/?INTCMP=Beta_PlansCatalog_CardCTAPrepaidPlans"},
       //Virgin
       { imgsrc: "assets/images/compare/virgin.png", name1: "Virgin Mobile", name2: "", plan: "Unlimited minutes + 2 GB",
-          minutes: "Unlimited in Canada", texts: "Unlimited from Canada",
+          minutes: "Unlimited in Canada", texts: "Unlimited from Canada", callRank: 7, textRank: 2,
           data: 2, price: 55, bonuses: "MyPeeps, voicemail 3, call display, Canada-Wide calling, unlimited weekends and evenings",
           link: "http://www.virginmobile.ca/en/plans/details.html?province=ON&geoResult=ON#!/postpaid/byop"}
   ];
