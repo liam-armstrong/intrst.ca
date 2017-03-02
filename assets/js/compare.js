7594c40ca1b6ebbbec8344cdf3a5644befbfb44d
ericpang777 <eric.pang@live.com>
2017-03-01 22:00:38 -0500

    Updated Titles

--- assets/js/compare.js
+++ assets/js/compare.js
@@ -25,7 +25,7 @@ app.controller("compareController", ["$scope", function($scope) {
             nonBankTrans: "transaction", prep: "Over", branches: 960,
             link: "https://www.bmo.com/main/personal/bank-accounts/smart-saver/"},
         //CIBC
-        { imgsrc: "assets/images/compare/CIBC.png", name1: "Advantage", name2: "for Youth", rate: 0.15,
+        { imgsrc: "assets/images/compare/CIBC.png", name1: "Advantage®", name2: "for Youth", rate: 0.15,
             eTransfer: "$1.50 per", eTrans: "transaction", transRank: 6, nonBankFees: "$1.50 per", atmRank: 4,
             nonBankTrans: "transaction", prep: "", branches: 1108,
             link: "https://www.cibc.com/en/personal-banking/bank-accounts/youth-banking-offers.html"},
@@ -35,17 +35,17 @@ app.controller("compareController", ["$scope", function($scope) {
             nonBankTrans: "transaction", prep: "Over", branches: 40,
             link: "http://www.cwbank.com/personal-banking/bank-accounts/youth-account"},
         //National Bank
-        { imgsrc: "assets/images/compare/NationalBank.png", name1: "Strategist", name2: "Account", rate: 0.02,
+        { imgsrc: "assets/images/compare/NationalBank.png", name1: "Strategist®", name2: "Account", rate: 0.02,
             eTransfer: "$1.25 per", eTrans: "transaction", transRank: 4, nonBankFees: "$0.75 per", atmRank: 1,
             nonBankTrans: "transaction", prep: "Around", branches: 450,
             link: "https://www.rbc.ca/en/personal/banking/saving-accounts/strategist-account.html"},
         //PC Financial
-        { imgsrc: "assets/images/compare/PCFinancial.png", name1: "Interest Plus", name2: "Savings Account", rate: 0.85,
+        { imgsrc: "assets/images/compare/PCFinancial.png", name1: "Interest Plus™", name2: "Savings Account", rate: 0.85,
             eTransfer: "$1.50 per", eTrans: "transaction", transRank: 8, nonBankFees: "$1.50 per", atmRank: 6,
             nonBankTrans: "transaction", prep: "", branches: 0,
             link: "http://www.banking.pcfinancial.ca/mkt/bankaccounts/interestplussavingsaccount-en.html"},
         //RBC
-        { imgsrc: "assets/images/compare/RBC.png", name1: "Leo's Young", name2: "Savers Account", rate: 0.01,
+        { imgsrc: "assets/images/compare/RBC.png", name1: "Leo's Young", name2: "Savers Account®", rate: 0.01,
           eTransfer: "Unlimited", eTrans: "", transRank: 2, nonBankFees: "$1.50 per", atmRank: 7,
           nonBankTrans: "transaction", prep: "Around", branches: 1200,
           link: "http://www.rbcroyalbank.com/products/deposits/leo-young-savers-account.html"},
@@ -67,25 +67,25 @@ app.controller("compareController", ["$scope", function($scope) {
     ];
 
     $scope.ccardBanks = [
-      { imgsrc: "assets/images/compare/Scotiabank.png", name: "Scotiabank Scene", fee: 0, limit: 500, features: ["4000 bonus signup bonus", "Sportchek discount", "1 scene point / $1 spent"],
+      { imgsrc: "assets/images/compare/Scotiabank.png", name: "Scotiabank® SCENE® VISA Card", fee: 0, limit: 500, features: ["4000 bonus signup bonus", "Sportchek discount", "1 scene point / $1 spent"],
           worth: 500, money: "500 Scene Points", link: "http://www.scotiabank.com/ca/en/0,,91,00.html" },
-      { imgsrc: "assets/images/compare/TD.png", name: "TD Bank Green", fee: 0, limit: 500, features: ["Basic Insurance provided (carrier insurance)"],
+      { imgsrc: "assets/images/compare/TD.png", name: "TD Green Visa Card", fee: 0, limit: 500, features: ["Basic Insurance provided (carrier insurance)"],
           worth: 0, money: "100,000 insurance", link: "https://www.tdcanadatrust.com/products-services/banking/credit-cards/view-all-cards/green-card.jsp" },
-      { imgsrc: "assets/images/compare/RBC.png", name: "RBC Cashback Mastercard", fee: 0, limit: 500, features: ["2% cashback on groceries", "1% on every dollar spent"],
+      { imgsrc: "assets/images/compare/RBC.png", name: "RBC® Cash Back Mastercard", fee: 0, limit: 500, features: ["2% cashback on groceries", "1% on every dollar spent"],
           worth: 0, money: "5 dollars back", link: "http://www.rbcroyalbank.com/credit-cards/cash-back-credit-cards/cash-back-mastercard/" },
-      { imgsrc: "assets/images/compare/BMO.png", name: "BMO SPC Cashback", fee: 0, limit: 500, features: ["SPC Card Benefits", "1% cashback on purchases", "BONUS - $60 first year"],
+      { imgsrc: "assets/images/compare/BMO.png", name: "BMO SPC CashBack Mastercard®", fee: 0, limit: 500, features: ["SPC Card Benefits", "1% cashback on purchases", "BONUS - $60 first year"],
           worth: 0, money: "5 dollars back", link: "https://www.bmo.com/main/personal/credit-cards/spc-cashback-student-credit-cards#"},
-      { imgsrc: "assets/images/compare/CIBC.png", name: "Tims Double Double Visa Card", fee: 0, limit: 500, features: ["1% tims ($20 signing bonus)"], worth: "N/A",
+      { imgsrc: "assets/images/compare/CIBC.png", name: "CIBC Tim Hortons® Double Double™ Visa Card*" , fee: 0, limit: 500, features: ["1% tims ($20 signing bonus)"], worth: "N/A",
           money: "5 Tim Hortons cash back", link: "https://www.cibc.com/en/personal-banking/credit-cards/retail-rewards-cards/tim-hortons-rewards-card.html"},
-      { imgsrc: "assets/images/compare/CIBC.png", name: "Classic Visa", fee: 0, limit: 500, features: ["100,000$ basic insurance"], worth: 0,
+      { imgsrc: "assets/images/compare/CIBC.png", name: "CIBC Classic Visa Card*", fee: 0, limit: 500, features: ["100,000$ basic insurance"], worth: 0,
           money: "100,000 insurance", link: "https://www.cibc.com/en/personal-banking/credit-cards/everyday-cards/classic-visa.html"},
-      { imgsrc: "assets/images/compare/CIBC.png", name: "Dividend Visa", fee: 0, limit: 500, features: ["1% everything / 2% groceries"], worth: "N/A",
+      { imgsrc: "assets/images/compare/CIBC.png", name: "CIBC Dividend® Visa Card*", fee: 0, limit: 500, features: ["1% everything / 2% groceries"], worth: "N/A",
           money: "5 dollars back", link: "https://www.cibc.com/ca/visa/dividend-card.html"},
-      { imgsrc: "assets/images/compare/Tangerine.png", name: "Tangerine", fee: 0, limit: "TBD", features: ["2% on select purchases, 1% on everything else"], worth: "N/A",
+      { imgsrc: "assets/images/compare/Tangerine.png", name: "Tangerine Money-Back Credit Card", fee: 0, limit: "TBD", features: ["2% on select purchases, 1% on everything else"], worth: "N/A",
           money: "5 dollars back", link: "https://www.tangerine.ca/moneybackcreditcard"},
-      { imgsrc: "assets/images/compare/mbna.png", name: "MBNA ", fee: 0, limit: 500, features: ["1 MBNA point / $1 spent"], worth: 500,
+      { imgsrc: "assets/images/compare/mbna.png", name: "MBNA Rewards MasterCard® Credit Card", fee: 0, limit: 500, features: ["1 MBNA point / $1 spent"], worth: 500,
           money: "Redeemed for items", link: "https://rewards.mbna.ca/?src=DAAF11"},
-      { imgsrc: "assets/images/compare/NationalBank.png", name: "National Bank ", fee: 0, limit: "500+", features: ["1% cash back for instore purchases, 1.5% online"], worth: 0,
+      { imgsrc: "assets/images/compare/NationalBank.png", name: "ECHO® CASHBACK MASTERCARD®", fee: 0, limit: "500+", features: ["1% cash back for instore purchases, 1.5% online"], worth: 0,
           money: "N/A", link: "https://www.nbc.ca/en/personal/credit-cards/mastercard-credit-cards/cashback-cards/echo.html" }
     ];
 
